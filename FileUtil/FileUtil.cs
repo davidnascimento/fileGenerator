046aa2debe69a0a35c05f2e9899ee8df35fd7cef
David Nascimento <davidfonascimento@gmail.com>
2019-12-12 23:55:25 -0300

    Criaçao da estrutura inicial

---
+++ FileUtil/FileUtil.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace FileUtil
+{
+    public class FileUtil
+    {
+        readonly string _path;
+        readonly string _fileName;
+
+        public FileUtil(string path)
+        { 
+            Directory.CreateDirectory(path);
+            _path = path;
+            _fileName = $"\\{DateTime.Now.ToString("YYYY-MM-DD-HHmmss")}-arquivo-gerado.txt";
+        }
+
+        public void WriteFile(string text)
+        {
+            using (StreamWriter stream = File.AppendText(_path + _fileName))
+            {
+                stream.Write(text);
+            }
+        }
+    }
+}
