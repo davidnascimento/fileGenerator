1fe55852afdc7692d869b18e035de28bb2d851bb
David Nascimento <davidfonascimento@gmail.com>
2019-12-13 00:09:37 -0300

    Input das variaveis

--- fileGeneratorApp/Program.cs
+++ fileGeneratorApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace fileGeneratorApp
 {
@@ -6,7 +7,36 @@ namespace fileGeneratorApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Informe o tamanho do arquivo a ser gerado! Default(100MB) ");
+            int.TryParse(Console.ReadLine(), out int fileSize);
+
+            if (fileSize == 0)
+            {
+                Console.WriteLine("Tamanho de arquivo invalido! Sera considerado o valor default 100MB!");
+                fileSize = 100;
+            }
+
+            Console.WriteLine("Informe o tamanho do buffer! Default(1MB)");
+            int.TryParse(Console.ReadLine(), out int bufferSize);
+
+            if (bufferSize == 0)
+            {
+                Console.WriteLine("Tamanho do buffer invalido! Sera considerado o valor default 1MB!");
+                bufferSize = 1;
+            }
+
+            string path;
+            var existPath = false;
+            while (!existPath)
+            {
+                Console.WriteLine("Informe o caminho para geracao do arquivo!");
+                path = Console.ReadLine();
+
+                if (!Directory.Exists(path))
+                    Console.WriteLine("Caminho invalido, favor informar um caminho valido!");
+            }
+
+            Console.ReadKey();
         }
     }
 }
