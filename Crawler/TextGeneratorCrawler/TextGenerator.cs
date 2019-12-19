0df1ccea5f879341c806c43198a2724227b79975
David Nascimento <davidfonascimento@gmail.com>
2019-12-18 23:14:24 -0300

    Criando projeto para crawler do site LeroLero com estrategia de fallback utilizando Polly

---
+++ Crawler/TextGeneratorCrawler/TextGenerator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using CrawlerCore;
+using Polly;
+
+namespace TextGeneratorCrawler
+{
+    public class TextGenerator : Crawler
+    {
+        private string _url = "https://lerolero.com";
+        private string _jsSelector = @"document.querySelector('.sentence').innerText";
+
+        public TextGenerator()
+        {
+        }
+
+        public TextGenerator(string url, string jsSelector)
+        {
+            _url = url;
+            _jsSelector = jsSelector;
+        }
+
+        public string GetText()
+        {
+            var policyFallbackForAnyException =
+                Policy<string>
+                .Handle<Exception>()
+                .OrResult(r => string.IsNullOrWhiteSpace(r))
+                .Fallback(() => GerarLeroLero());
+
+            return policyFallbackForAnyException.Execute(() => GetTextApi());
+        }
+
+        private string GetTextApi()
+        {
+            return ExecuteJsCommand<string>(_url, _jsSelector);
+        }
+
+        private string GerarLeroLero()
+        {
+            return GeradorLeroLero.GenerateSentence();
+        }
+    }
+}
