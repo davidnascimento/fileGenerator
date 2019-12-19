4d0cb156062811ec670be675846ad8c7c2e79bc0
David Nascimento <davidfonascimento@gmail.com>
2019-12-18 22:55:06 -0300

    Criando projeto base do crawler utilizando puppeteersharp

---
+++ Crawler/CrawlerCore/Crawler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using PuppeteerSharp;
+
+namespace CrawlerCore
+{
+    public class Crawler
+    {
+        public Crawler()
+        {
+            _ = new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision).Result;
+        }
+
+        public T ExecuteJsCommand<T>(string url, string jsCommand)
+        {
+            var options = new LaunchOptions { Headless = true };
+            using (var browser = Puppeteer.LaunchAsync(options).Result)
+            {
+                using (var page = browser.NewPageAsync().Result)
+                {
+                    _ = page.GoToAsync(url).Result;
+                    
+                    return page.EvaluateExpressionAsync<T>(jsCommand).Result;
+                    //var jsSelector = @"document.querySelector('#bytes').innerText";
+                    //Int32.TryParse(Regex.Match(innerText, @"\d+").Value, out int count);
+                }
+            }
+        }
+    }
+}
